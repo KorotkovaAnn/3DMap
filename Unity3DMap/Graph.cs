c54899c5f2673840e774d9f7c1fbe9a4ffea83b1
анечка <korotkova09121994@gmail.com>
2024-03-18 21:04:15 +0500

    Hello

---
+++ Unity3DMap/Graph.cs
@@ -0,0 +1,56 @@
+﻿using System;
+namespace Unity3DMap
+{
+    public class Graph
+    {
+        public List<Point> points = new List<Point>();
+        public List<Edge> edges = new List<Edge>();
+
+        public double[,] Matrix;
+
+        public Graph(List<Point> points, List<Edge> edges)
+        {
+            this.points = points;
+            this.edges = edges;
+            Matrix = new double[points.Count, points.Count];
+        }
+
+        public void CreateGraph()
+        {
+            for (int i = 0; i < points.Count; i++)
+            {
+                for (int j = 0; j < points.Count; j++)
+                {
+                    var edge = edges.FirstOrDefault(e =>
+                    {
+                        return e.First == points[i] && e.Second == points[j] || e.Second == points[i] && e.First == points[j];
+                    });
+                    if (edge != null)
+                    {
+                        Matrix[i, j] = edge.Length;
+                    }
+                    else
+                    {
+                        Matrix[i, j] = 0;
+                    }
+
+                }
+            }
+        }
+
+        public void PrintGraph()
+        {
+            for (int i = 0; i < points.Count; i++)
+            {
+                for (int j = 0; j < points.Count; j++)
+                {
+                    Console.Write(string.Format("{0:f1}", Matrix[i, j] + "\t"));
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
+
---
+++ Unity3DMap/Program.cs
@@ -0,0 +1,36 @@
+﻿using Unity3DMap;
+
+public class Program
+{
+    public static void Main()
+    {
+        List<Point> points = new List<Point>()
+        {
+            new Point(0, 0),
+            new Point(0, 2),
+            new Point(0, 4),
+            new Point(1, 1),
+            new Point(3, 3),
+
+        };
+        List<Edge> edges = new List<Edge>()
+        {
+            new Edge(points[0], points[3]),
+            new Edge(points[0], points[1]),
+            new Edge(points[3], points[1]),
+            new Edge(points[1], points[2]),
+            new Edge(points[1], points[4]),
+            new Edge(points[2], points[4]),
+        };
+
+        Graph graph = new Graph(points, edges);
+        graph.CreateGraph();
+        graph.PrintGraph();
+
+        Algorithm algorithm = new Algorithm(graph, points[0], points[4]);
+        double shortdistance = algorithm.Dijkstra();
+        Console.WriteLine($"Кратчайшее расстояние между точками: {shortdistance}");
+        Console.ReadLine();
+    }
+}
+
